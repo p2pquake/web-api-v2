071ac367e7df4c008270e3a1e087e5d1d033d75c
typewriter <takuyaxp@gmail.com>
2019-08-20 01:16:43 +0900

    Implement unconditional query

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ type Config struct {
 	Collection string `json:"collection"`
 }
 
+var collection *mongo.Collection
+
 func main() {
 	file, err := ioutil.ReadFile("config.json")
 	if err != nil { return }
@@ -36,23 +38,68 @@ func main() {
 	if err != nil { return }
 	defer client.Disconnect(ctx)
 
-	collection := client.Database(config.Database).Collection(config.Collection)
+	collection = client.Database(config.Database).Collection(config.Collection)
 
 	r := gin.Default()
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "こんにちは世界",
-		})
-	})
-
-	r.GET("/count", func(c *gin.Context) {
-		count, err := collection.CountDocuments(ctx, bson.D{{}})
-		if err != nil { count = -1 }
-		c.JSON(200, gin.H{
-			"count": count,
-		})
-	})
+	v2 := r.Group("/v2")
+	{
+		jma := v2.Group("/jma")
+		{
+			jma.GET("/quake", quakeEndpoint)
+			jma.GET("/tsunami", tsunamiEndpoint)
+		}
+	}
 
 	r.Run()
+}
+
+func quakeEndpoint(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	filter := bson.D{{"code", 551}}
+	limit := int64(10)
+	options := options.FindOptions{ Limit: &limit }
+
+	cur, err := collection.Find(ctx, filter, &options)
+	if err != nil { return }
+	defer cur.Close(ctx)
+
+	var items []bson.M
+	cur.All(ctx, &items)
+
+	for _, item := range items {
+		cleanJmaRecord(item)
+	}
+
+  c.JSON(200, items)
+}
+
+func tsunamiEndpoint(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	filter := bson.D{{"code", 552}}
+	limit := int64(10)
+	options := options.FindOptions{ Limit: &limit }
+
+	cur, err := collection.Find(ctx, filter, &options)
+	if err != nil { return }
+	defer cur.Close(ctx)
+
+	var items []bson.M
+	cur.All(ctx, &items)
+
+	for _, item := range items {
+		cleanJmaRecord(item)
+	}
+
+  c.JSON(200, items)
+}
+
+func cleanJmaRecord(m bson.M) {
+	m["id"] = m["_id"]
+	delete(m, "_id")
+	delete(m, "expire")
 }
