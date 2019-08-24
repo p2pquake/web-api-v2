ddf2afab1d7b257bbead9e1acc15995dfba38b45
typewriter <takuyaxp@gmail.com>
2019-08-24 13:45:22 +0900

    Add validations

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"context"
 	"encoding/json"
 	"io/ioutil"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/go-playground/validator.v8"
 )
 
 type Config struct {
@@ -22,7 +25,47 @@ type Config struct {
 	Collection string `json:"collection"`
 }
 
+type QuakeParam struct {
+	Offset       int64    `form:"offset" binding:"min=0"`
+	Limit        int64    `form:"limit" binding:"min=0,max=100"`
+	QuakeType    string   `form:"quake_type" binding:"omitempty,quaketype"`
+	MinScale     int64    `form:"min_scale" binding:"omitempty,scale"`
+	MaxScale     int64    `form:"max_scale" binding:"omitempty,scale"`
+	MinMagnitude float64  `form:"min_magnitude" binding:"min=0.0"`
+	MaxMagnitude float64  `form:"max_magnitude" binding:"min=0.0"`
+	SinceDate    string   `form:"since_date" binding:"omitempty,numeric,len=8"`
+	UntilDate    string   `form:"until_date" binding:"omitempty,numeric,len=8"`
+	Prefectures  []string `form:"prefectures[]" binding:"omitempty,dive,contains=0x2C"`
+}
+
 var collection *mongo.Collection
+
+func validQuakeType(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	if quakeType, ok := field.Interface().(string); ok {
+		if quakeType == "ScalePrompt" || quakeType == "Destination" ||
+			quakeType == "ScaleAndDestination" || quakeType == "DetailScale" ||
+			quakeType == "Foreign" || quakeType == "Other" {
+			return true
+		}
+	}
+	return false
+}
+
+func validScale(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	if scale, ok := field.Interface().(int64); ok {
+		if scale == 10 || scale == 20 || scale == 30 || scale == 40 ||
+			scale == 45 || scale == 50 || scale == 55 || scale == 60 || scale == 70 {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	file, err := ioutil.ReadFile("config.json")
@@ -52,6 +95,11 @@ func main() {
 
 	r := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("quaketype", validQuakeType)
+		v.RegisterValidation("scale", validScale)
+	}
+
 	v2 := r.Group("/v2")
 	{
 		jma := v2.Group("/jma")
@@ -66,51 +114,53 @@ func main() {
 }
 
 func searchQuake(c *gin.Context) {
+	var quakeParam QuakeParam
+	if err := c.ShouldBindWith(&quakeParam, binding.Query); err != nil {
+		c.Status(400)
+		return
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	// options
-	offset, _ := strconv.ParseInt(c.DefaultQuery("offset", "0"), 10, 64)
-	limit, _ := strconv.ParseInt(c.DefaultQuery("limit", "10"), 10, 64)
-	if limit <= 0 || limit > 100 {
-		c.Status(400)
-		return
+	offset := quakeParam.Offset
+	limit := quakeParam.Limit
+	if limit == 0 {
+		limit = 10
 	}
 	options := options.FindOptions{Limit: &limit, Skip: &offset}
 
 	// filters
 	filters := bson.D{{"code", 551}}
 
-	sinceDate := c.Query("since_date")
-	untilDate := c.Query("until_date")
 	dateRegexp := regexp.MustCompile(`^(\d{4})(\d{2})(\d{2})$`)
-	if matches := dateRegexp.FindStringSubmatch(sinceDate); matches != nil {
+	if matches := dateRegexp.FindStringSubmatch(quakeParam.SinceDate); matches != nil {
 		filters = append(filters, bson.E{"earthquake.time", bson.D{{"$gte", matches[1] + "/" + matches[2] + "/" + matches[3] + " 00:00:00"}}})
 	}
-	if matches := dateRegexp.FindStringSubmatch(untilDate); matches != nil {
+	if matches := dateRegexp.FindStringSubmatch(quakeParam.UntilDate); matches != nil {
 		filters = append(filters, bson.E{"earthquake.time", bson.D{{"$lte", matches[1] + "/" + matches[2] + "/" + matches[3] + " 23:59:59"}}})
 	}
 
-	if quakeType := c.Query("quake_type"); quakeType != "" {
-		filters = append(filters, bson.E{"issue.type", quakeType})
+	if quakeParam.QuakeType != "" {
+		filters = append(filters, bson.E{"issue.type", quakeParam.QuakeType})
 	}
-	if minMagnitude, err := strconv.ParseFloat(c.Query("min_magnitude"), 64); err == nil {
-		filters = append(filters, bson.E{"earthquake.hypocenter.magnitude", bson.D{{"$gte", minMagnitude}}})
+	if quakeParam.MinMagnitude != 0.0 {
+		filters = append(filters, bson.E{"earthquake.hypocenter.magnitude", bson.D{{"$gte", quakeParam.MinMagnitude}}})
 	}
-	if maxMagnitude, err := strconv.ParseFloat(c.Query("max_magnitude"), 64); err == nil {
-		filters = append(filters, bson.E{"earthquake.hypocenter.magnitude", bson.D{{"$lte", maxMagnitude}}})
+	if quakeParam.MaxMagnitude != 0.0 {
+		filters = append(filters, bson.E{"earthquake.hypocenter.magnitude", bson.D{{"$lte", quakeParam.MaxMagnitude}}})
 		filters = append(filters, bson.E{"earthquake.hypocenter.magnitude", bson.D{{"$gte", 0.0}}})
 	}
-	if minScale, err := strconv.Atoi(c.Query("min_scale")); err == nil {
-		filters = append(filters, bson.E{"earthquake.maxScale", bson.D{{"$gte", minScale}}})
+	if quakeParam.MinScale != 0 {
+		filters = append(filters, bson.E{"earthquake.maxScale", bson.D{{"$gte", quakeParam.MinScale}}})
 	}
-	if maxScale, err := strconv.Atoi(c.Query("max_scale")); err == nil {
-		filters = append(filters, bson.E{"earthquake.maxScale", bson.D{{"$lte", maxScale}}})
+	if quakeParam.MaxScale != 0 {
+		filters = append(filters, bson.E{"earthquake.maxScale", bson.D{{"$lte", quakeParam.MaxScale}}})
 		filters = append(filters, bson.E{"earthquake.maxScale", bson.D{{"$gte", 0}}})
 	}
 
-	prefectures, _ := c.GetQueryArray("prefectures[]")
-	for _, prefecture := range prefectures {
+	for _, prefecture := range quakeParam.Prefectures {
 		elements := strings.Split(prefecture, ",")
 		prefectureName := elements[0]
 		scale, _ := strconv.Atoi(elements[1])
